cc8915782e5b0fdb58cef37ff74de92f699faaba
Shubham Yadav <144605009+shubhamyadav638@users.noreply.github.com>
2024-10-22 16:57:25 +0530

    Success

---
+++ src/Hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import xyz from './hello.module.css'
+
+function Hello() {
+  return (
+    <div>
+      <h2 className={xyz.abc}>this is hello component</h2>
+    </div>
+  )
+}
+
+export default Hello
